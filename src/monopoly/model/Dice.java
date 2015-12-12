6b32c7b08963e35007f8f0551b64f837a9e9892d
Oguz Parlak <goldbotb@gmail.com>
2015-12-12 14:21:15 +0200

    Dice Update
    Modification Dice

--- src/monopoly/model/Dice.java
+++ src/monopoly/model/Dice.java
@@ -1,11 +1,25 @@
 package monopoly.model;
+import java.util.Random;
+
 
 public class Dice {
-  
-  public int getDice() {
-		Random random = new Random();
-		int dice = 1+rand.nextInt(6);
-		return dice ;
-	}
-
+    
+    private int faceValue=0;
+    
+    public static final int MAXVALUE=6;
+    public static final int MINVALUE=1;
+    
+    private Random rand = new Random();
+    
+    public Dice(){
+        
+    }
+    
+    public void roll(){
+        faceValue = rand.nextInt(MAXVALUE) + MINVALUE;
+    }
+    
+    public int getValue(){
+        return faceValue;
+    }
 }
