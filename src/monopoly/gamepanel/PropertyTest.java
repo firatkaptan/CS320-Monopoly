e0833dcd10c9d29bb9fef52535404c7576e52b48
mertozkan <mert.ozkan@ozu.edu.tr>
2015-12-13 13:51:52 +0200

    Create PropertyTest.java

---
+++ src/monopoly/gamepanel/PropertyTest.java
@@ -0,0 +1,88 @@
+package monopoly.gamepanel;
+
+import monopoly.model.Player;
+import monopoly.model.Token;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.awt.*;
+
+import static org.junit.Assert.*;
+
+
+public class PropertyTest {
+Property property;
+    Property property2;
+    int buildings;
+    int x;
+    int y;
+    Color color;
+    String name;
+    int price;
+    int rent;
+    int buildCost;
+    Player player;
+    Player owner;
+    Token token;
+    Graphics g;
+    @Before
+    public void setUp() throws Exception {
+
+
+        player=new Player(name,token);
+        owner=new Player(name,token);
+        property=new Property(x,y,color,name,price,rent,buildCost);
+        property2=new Property(x,y,color,name,price,rent,buildCost);
+    }
+
+    @Test
+    public void testBuild() throws Exception {
+
+
+        property.build(buildings);
+        int a=3;
+        buildings=3;
+        assertTrue(6==a+buildings);
+    }
+
+    @Test
+    public void testSetOwner() throws Exception {
+property.setOwner(player);
+        owner=player;
+        assertTrue(player==owner);
+
+    }
+
+    @Test
+    public void testGetCost() throws Exception {
+
+        property.getCost();
+        property2.getCost();
+        assertTrue(property.getCost()==property2.getCost());
+
+    }
+
+    @Test
+    public void testGetRent() throws Exception {
+
+        property.getRent();
+        property2.getRent();
+
+        assertTrue(property.getRent()==property2.getRent());
+    }
+
+    @Test
+    public void testGetPrice() throws Exception {
+
+        property.getPrice();
+        property2.getPrice();
+        assertTrue(property.getPrice()==property2.getPrice());
+    }
+
+    @Test
+    public void testPaint() throws Exception {
+
+
+
+    }
+}
