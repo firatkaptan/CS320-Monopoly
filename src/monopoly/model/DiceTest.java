9305ec3ae5a02164f396d45de66e9796afa2ae16
mertozkan <mert.ozkan@ozu.edu.tr>
2015-12-13 15:29:16 +0200

    Create DiceTest.java

---
+++ src/monopoly/model/DiceTest.java
@@ -0,0 +1,40 @@
+package monopoly.model;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Random;
+
+import static org.junit.Assert.*;
+
+
+public class DiceTest {
+
+    Dice dice;
+    int MAXVALUE=6;
+    int MINVALUE=1;
+    Random random;
+
+    @Before
+    public void setUp() throws Exception {
+
+        random=new Random();
+        dice =new Dice();
+    }
+
+    @Test
+    public void testRoll() throws Exception {
+
+        dice.roll();
+        int faceValue=5;
+        assertTrue(faceValue==5);
+    }
+
+    @Test
+    public void testGetValue() throws Exception {
+
+        dice.getValue();
+        int faceValue=6;
+        assertTrue(faceValue==6);
+    }
+}
